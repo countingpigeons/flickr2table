b0ee2dfcf8567395c4d1470ce234b32090994ac7
countingpigeons <49214807+countingpigeons@users.noreply.github.com>
2020-08-06 20:15:40 -0700

    added airtable.py, stable export, initial upload

--- airtable_upload.py
+++ airtable_upload.py
@@ -1,28 +1,110 @@
-import requests
 import json
 from counting_pigeons_config import AirTableConfig
+import airtable as at
+
+
+def pretty_json(dict):
+    json_string = json.dumps(dict, indent=2, sort_keys=True)
+    return json_string
+
 
 apikey = AirTableConfig.apikey
-flora_base_id = AirTableConfig.flora_base_id
+base_id = AirTableConfig.development_base_id
+# base_id = AirTableConfig.flora_base_id
 
-auth_headers = {'Content-type': 'application/json',
-                'Authorization': f'Bearer {apikey}'}
+airtable = at.Airtable(base_id, apikey, dict)
+table_name = 'yosemite'
+
+# print(help(airtable))
+
+# result = airtable.get(table_name)  # , 'rec8TbKVkYBcx6qE8')
+# print(len(result['records']))
+# # print(pretty_json(result))
+#
+# record_id = -1
+# for record in result['records']:
+#     flickr_id = record['fields']['Flickr_id']
+#     if flickr_id == '456':
+#         record_id = record['id']
+# if record_id != -1:
+#     # print(f'record_id: {record_id}, flickr_id: {flickr_id}, row_id: {record_id}')
+#     print(f'record_id: {record_id}')
+
+# bad_record = airtable.get(table_name, record_id)
+# print(pretty_json(bad_record))
+# bad_record = airtable.delete(table_name, record_id)
+# print(bad_record)
+
+# record to copy
+# record_id = -1
+# for record in result['records']:
+#     flickr_id = record['fields']['Flickr_id']
+#     if flickr_id == '123':
+#         record_id = record['id']
+#
+# copy_record = airtable.get(table_name, record_id)
+# copy_record['fields']['Name'] = 'NewRowInserted'
+# del copy_record['id']
+
+# print(copy_record['Name'])
+# print(pretty_json(copy_record))
+# print(copy_record.keys())
+# print(copy_record['fields']['Name'])
+
+# records = []
+# records.append(copy_record)
+# payload = {"records": [{"fields": [{"Name": "thisname"}]}]}
+# print(help(json))
+# {"records": [records]}
+
+# payload = {"records": [
+#     {
+#         "fields": {
+#             "Name": "Jd n Logan [2]"}}]}
+
+data = {"Name": "AnotherName",
+        # "Image(s)": [{"url": "https://live.staticflickr.com/65535/49671531862_583e980814_o.jpg"},
+        #              {"url": "https://live.staticflickr.com/65535/49670721138_b23eccfeb9_o.jpg"}],
+        # "Color": "Yellow",
+        # "PlantType": ["Vine", "Flower"],
+        "Color": "Green",
+        "DateTaken": "2008-12-01"}
+
+# print(type(payload))
+print(pretty_json(data))
+
+upload = airtable.create(table_name, data=data)
+print(upload)
 
 
-def pretty_json(json_response) -> str:
-    pretty_string = json.dumps(json_response, indent=2, sort_keys=True)
-    return pretty_string
+# for field in record['fields']:
+#     print(field)
+#         if field['Flickr_id'] == '456':
+#             rowid = record['id']
+#         else:
+#             rowid = -1
+#
+# print(rowid)
 
-
-# url = ('https://api.airtable.com/v0/appl1aRzPZnabDFpM'
-#        '/FLORA?maxRecords=3&view=Grid%20view')
-url = ('https://api.airtable.com/v0/appCXvmzt4Nmc8vyE'
-       '/yosemite?maxRecords=3&view=Grid%20view')
-development_base = 'appCXvmzt4Nmc8vyE'
-
-response = requests.get(url, headers=auth_headers)
-
-# print(type(response))
-# print(response.json()['records'])
-
-print(pretty_json(response.json()))
+# PREV WORKS
+# auth_headers = {'Content-type': 'application/json',
+#                 'Authorization': f'Bearer {apikey}'}
+#
+#
+# def pretty_json(json_response) -> str:
+#     pretty_string = json.dumps(json_response, indent=2, sort_keys=True)
+#     return pretty_string
+#
+#
+# # url = ('https://api.airtable.com/v0/appl1aRzPZnabDFpM'
+# #        '/FLORA?maxRecords=3&view=Grid%20view')
+# url = ('https://api.airtable.com/v0/appCXvmzt4Nmc8vyE'
+#        '/yosemite?maxRecords=3&view=Grid%20view')
+# development_base = 'appCXvmzt4Nmc8vyE'
+#
+# response = requests.get(url, headers=auth_headers)
+#
+# # print(type(response))
+# # print(response.json()['records'])
+#
+# print(pretty_json(response.json()))
--- flickr_export.py
+++ flickr_export.py
@@ -1,8 +1,10 @@
 from counting_pigeons_config import FlickrConfig
 from requests_oauthlib import OAuth1Session
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
+import argparse
+import re
 
 base_api_url = 'https://www.flickr.com/services/rest/?method=flickr.'
 json_url_suffix = '&format=json&nojsoncallback=1'
@@ -10,8 +12,8 @@ json_url_suffix = '&format=json&nojsoncallback=1'
 oauth = OAuth1Session(
     client_secret=FlickrConfig.secret,
     client_key=FlickrConfig.apikey,
-    resource_owner_key=FlickrConfig.resource_owner_key_brian,
-    resource_owner_secret=FlickrConfig.resource_owner_secret_brian)
+    resource_owner_key=FlickrConfig.resource_owner_key_karen,
+    resource_owner_secret=FlickrConfig.resource_owner_secret_karen)
 
 
 def api_call(method: str, params: dict = {}) -> json:
@@ -63,10 +65,13 @@ def get_album_details(user_id, album_name: str):
     return None
 
 
-def get_album_photos_json(user_id, photoset_id):
+def get_album_photos_json(user_id, photoset_id, per_page=200, page=1):
     method = 'photosets.getPhotos'
     extras_string = 'geo,url_o,date_taken,date_upload,last_update,tags'
-    params = {"user_id": user_id, "photoset_id": photoset_id, "extras": extras_string}
+    params = {"user_id": user_id, "photoset_id": photoset_id,
+              "extras": extras_string, "per_page": str(per_page),
+              "page": str(page)}
+
     response = api_call(method, params)
     if response['stat'] == 'ok':
         response = response['photoset']
@@ -100,18 +105,26 @@ def get_photo_info_json(photo_id, feature=None):
         return response
 
 
-def create_album_output(run_date, album_name: str) -> str:
+def create_album_output(run_date, album_name: str, backfill=False) -> str:
+    cutoff_7_days = datetime.today()-timedelta(days=7)
     user_id = get_user_id()
     album_id, album_title, album_num_photos, album_created, album_updated =\
-        get_album_details(user_id, album_name)  # "yosemite '16")
-
-    album_photos = get_album_photos_json(user_id,
-                                         photoset_id=album_id)['photo']
-    keys = ['datetaken', 'dateupload', 'description', 'id', 'lastupdate',
+        get_album_details(user_id, album_name)
+    photos_per_page = 200
+    pages = (album_num_photos // photos_per_page)
+    if photos_per_page * pages < album_num_photos:
+        pages += 1
+    album_photos = []
+    for page in range(1, pages+1):
+        page_photos = get_album_photos_json(
+            user_id,
+            photoset_id=album_id,
+            per_page=photos_per_page,
+            page=page)['photo']
+        album_photos.extend(page_photos)
+    keys = ['datetaken', 'dateupload', 'id', 'lastupdate',
             'latitude', 'longitude', 'tags', 'title', 'url_o', 'flickr_url']
-    flickr_in_album_base_url = ('https://www.flickr.com/photos/{}/'
-                                '{}/in/album-{}/')
-    new_album_photos = []
+    filtered_album_photos = []
     album = {}
     album_latest_photo_update = '0'
     for photo in album_photos:
@@ -122,15 +135,31 @@ def create_album_output(run_date, album_name: str) -> str:
             int(photo['dateupload'])).date().strftime('%Y-%m-%d')
         photo['lastupdate'] = datetime.fromtimestamp(
             int(photo['lastupdate'])).date().strftime('%Y-%m-%d')
-        photo['description'] = get_photo_info_json(photo_id, 'description')
+        flickr_in_album_base_url = ('https://www.flickr.com/photos/{}/'
+                                    '{}/in/album-{}/')
         photo['flickr_url'] = \
             flickr_in_album_base_url.format(user_id, photo_id, album_id)
-        photo2 = {mykey: photo[mykey] for mykey in keys}
-        new_album_photos.append(photo2)
-    album['photos'] = new_album_photos
+        clean_photo = {mykey: photo[mykey] for mykey in keys}
+        if backfill:
+            cutoff = datetime.strptime('2010-01-01', '%Y-%m-%d')
+        else:
+            cutoff = cutoff_7_days
+
+        fresh = datetime.strptime(
+            clean_photo['lastupdate'], '%Y-%m-%d') > cutoff
+        tbd = re.search('tbd', clean_photo['title'])
+        if fresh and not tbd:
+            clean_photo['description'] = get_photo_info_json(
+                photo_id, 'description')
+            filtered_album_photos.append(clean_photo)
+
+    album['photos'] = filtered_album_photos
     album['id'] = album_id
     album['title'] = album_title
-    album['num_photos'] = album_num_photos
+    if backfill:
+        album['num_photos'] = album_num_photos
+    else:
+        album['num_photos'] = len(album['photos'])
     album['created'] = album_created
     album['updated'] = album_updated
     album['run_date'] = run_date
@@ -140,10 +169,10 @@ def create_album_output(run_date, album_name: str) -> str:
     return pretty_json(album)
 
 
-def write_album_output(run_date, album):
-    base_file_name = 'flickr_album_{}.json'
+def write_album_output(run_date, album_name, album):
+    base_file_name = 'flickr_album_{}_{}.json'
     file_path = '/home/koya/datascience/flickr_to_airtable/flickr_exports/'
-    file_name = base_file_name.format(run_date)
+    file_name = base_file_name.format(album_name, run_date)
     full_path = file_path + file_name
     with open(full_path, 'w') as f:
         f.write(album)
@@ -151,25 +180,27 @@ def write_album_output(run_date, album):
     return 1
 
 
-def main(album_name):
+def main():
+
     run_date = datetime.today().date().strftime('%Y-%m-%d')
-    album = create_album_output(run_date, album_name)
-    # # "yosemite '16")
-    export_success = write_album_output(run_date, album)
-    # print(album)
-    # print(run_date, album_name)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--album_name",
+                        help="name as it appears in Flickr UI' "
+                        "(default \'Flora\')",
+                        default='Flora')
+    parser.add_argument("--backfill", help="import all photos from album",
+                        action="store_true")
+    args = parser.parse_args()
+    # print(f'album_name: {args.album_name}, backfill: {args.backfill}')
+
+    album = create_album_output(run_date, args.album_name, args.backfill)
+    export_success = write_album_output(run_date, args.album_name, album)
+
     return export_success
 
 
 if __name__ == '__main__':
-
-    try:
-        album_name = sys.argv[1]
-    except IndexError as e:
-        album_name = "yosemite '16"
-        print(f'used default album. error: "{e}"')
-
-    main(album_name)
+    main()
 
 
 # print(pretty_json(get_albums_json(user_id)))
